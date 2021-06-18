beb6996031351962619c16043c6e4e64a1508912
isaacW <ISAAC.WILSON1811@STU.MTEC.EDU>
2021-06-17 18:30:59 -0600

    fixed db close

--- backend/api.js
+++ backend/api.js
@@ -1,25 +1,32 @@
+// modules
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const { MongoClient } = require('mongodb');
-const uri = "mongodb://localhost:27017/";
-const dbName = 'isaacs_db';
-const collectionName = 'users';
-const MONGO = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
+// vars
+const uri = "mongodb://localhost:27017/"; // local mongodb uri
+const dbName = 'isaacs_db'; // database name
+const colName = 'users'; // collection name
+
+// middleware
 app.use(express.json());
 
+// setup mongo driver
+const MONGO = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// routes
 app.get('/users/all', (req, res) => {
-  try {
-    MONGO.connect( async (err, mongo) => {
-      if (err) throw err;
-      const db = mongo.db(dbName);
-      let result = await db.collection(collectionName).find({}).toArray();
-      res.json(result);
-      db.close();
-    });
-  }
-  catch (err) {res.status(500).json({message:err.message})};
+  MONGO.connect( async (err, mongo) => {
+    if (err) {
+      res.status(500).json({message:err.message});
+      throw err;
+    }
+    const db = mongo.db(dbName);
+    let result = await db.collection(colName).find({}).toArray();
+    res.json(result);
+    MONGO.close();
+  });
 });
 
 // get one user by id
@@ -39,6 +46,7 @@ app.delete('/:id', (req, res) => {
 
 });
 
+// start server
 app.listen(port, () => {
   console.log(`Server Listening on port ${port}`)
 });
